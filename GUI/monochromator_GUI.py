da0357c16d665632c0a104fcf75dd5b65f8a4f8b
hebecked <dustin.hebecker@gmail.com>
2015-08-11 18:23:50 +0200

    A test

--- GUI/monochromator_GUI.py
+++ GUI/monochromator_GUI.py
@@ -7,7 +7,7 @@
 #
 # WARNING! All changes made in this file will be lost!
 
-from PyQt5 import QtCore, QtGui
+from PyQt4 import QtCore, QtGui
 import sys
 sys.path.append("..")
 from monochromator import CornerStone260
