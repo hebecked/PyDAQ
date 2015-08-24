8735d12be4a67e7ea091f92449699abd9cb25518
hebecked <dustin.hebecker@gmail.com>
2015-08-24 20:36:15 +0200

    debuging lockin.py

--- lockIn.py
+++ lockIn.py
@@ -5,7 +5,7 @@ import time
 
 class lockin:
 
-	def __init__(self,port, avrgn=10, autogain=False):
+	def __init__(self,port, avrgn=10, autogain=False, timeconstant=-1):
 		self.port=port#...
 		self.avrgn=avrgn
 		self.autogain=autogain
@@ -13,6 +13,10 @@ class lockin:
 		self.timeout=1
 		self.bytesize=8
 		self.sendtermchar='\n'
+		if timeconstant==-1:
+			self.timeconstant=self.get_timeconstant()
+		else:
+			self.timeconstant=self.set_timeconstant(timeconstant)
 		
 	def SerialCommand(self,command):
 		#setup - if a Serial object can't be created, a SerialException will be raised.
@@ -26,7 +30,7 @@ class lockin:
 				time.sleep(3)
 		self.ser.flushInput()        
 		self.ser.write(command + self.sendtermchar)
-		answer = self._read_LI()
+		#answer = self._read_LI()
 		self.ser.close()
 		return
 
@@ -50,7 +54,7 @@ class lockin:
 		list_=[]
 		help=0
 		while help!='\r':
-			print help
+			#print help
 			help=self.ser.read()
 			list_.append(help)
 		return ''.join(list_[0:-1])
@@ -74,8 +78,9 @@ class lockin:
 		for i in range(0,N):  
 			self.ser.flushInput()    
 			self.ser.write('OUTP?3' + self.sendtermchar)
-			ampl.append(float(self._read_LI()))
-			print float(self._read_LI())
+			help=float(self._read_LI())
+			ampl.append(help)
+			#print help
 			time.sleep(1)
 			self.ser.flushInput()
 			self.ser.write('OUTP?4' + self.sendtermchar)
@@ -90,14 +95,14 @@ class lockin:
 
 
  	def AutoGain(self):
- 		list_=[2e-9,5e-9,1e-8,2e-8,5e-8,1e-7,2e-7,5e-7,1e-6,2e-6,5e-6,1e-5,2e-5,5e-5,1e-4,2e-4,5e-4,1e-3,2e-3,5e-3,1e-2,2e-2,5e-2,1e-1,2e-1,5e-1,1]
+ 		list_=[2e-15,5e-15,1e-14,2e-14,5e-14,1e-13,2e-13,5e-13,1e-12,2e-12,5e-12,1e-11,2e-11,5e-11,1e-10,2e-10,5e-10,1e-9,2e-9,5e-9,1e-8,2e-8,5e-8,1e-7,2e-7,5e-7,1e-6]
  		index_old=2
  		change=20
  		while change!=0:
  			temp=self.SerialQuery('OUTP?3')
  			print temp
  			value=float(temp)
- 			index=np.argmin(np.abs(value-np.array(list_)))
+ 			index=np.argmin(np.abs(value-np.asarray(list_)))
  			if value>list_[index]:
  				if index<26:
  					index=index+1
@@ -113,7 +118,8 @@ class lockin:
 		return self.SerialQuery('OUTP?4')
 
 
-	def set_range(self,range):
+	def set_gain(self,range):
+		list_=[2e-15,5e-15,1e-14,2e-14,5e-14,1e-13,2e-13,5e-13,1e-12,2e-12,5e-12,1e-11,2e-11,5e-11,1e-10,2e-10,5e-10,1e-9,2e-9,5e-9,1e-8,2e-8,5e-8,1e-7,2e-7,5e-7,1e-6]
 		self.ser.write()
 		self.range=range
 
@@ -121,23 +127,30 @@ class lockin:
 	def get_frequency(self):
 		return self.SerialQuery('FREQ?')
 
+	def get_amplitude(self):
+		return self.SerialQuery('OUTP?3')
+
+	def get_phase(self):
+		return self.SerialQuery('OUTP?4')
+
 
 	def get_timeconstant(self):
 		index=int(self.SerialQuery('OFLT?'))
-		list_=[1e-6,3e-6,1e-5,3e-5,1e-4,3e-4,1e-3,5e-3,1e-2,3e-2,1e-1,3e-1,1,3,10,30,100,300,1e+3,3e+3,1e+4,3e+4]
+		list_=[1e-5,3e-5,1e-4,3e-4,1e-3,3e-3,1e-2,3e-2,1e-1,3e-1,1,3,10,30,100,300,1e+3,3e+3,1e+4,3e+4]
+		self.timeconstant=list_[index]
 		return list_[index]
 
-	def set_timeconstant(timeconstant):
-		list_=[1e-6,3e-6,1e-5,3e-5,1e-4,3e-4,1e-3,5e-3,1e-2,3e-2,1e-1,3e-1,1,3,10,30,100,300,1e+3,3e+3,1e+4,3e+4]
-		index=np.argmin(np.abs(list_-timeconstant))
+	def set_timeconstant(self,timeconst):
+		list_=[1e-5,3e-5,1e-4,3e-4,1e-3,3e-3,1e-2,3e-2,1e-1,3e-1,1,3,10,30,100,300,1e+3,3e+3,1e+4,3e+4]
+		index=np.argmin(np.abs(np.asarray(list_)-timeconst))
 		self.timeconstant=list_[index]
 		self.SerialCommand('OFLT' + str(index))
 
 
-	def autoset_timeconstant(periodes=100.):
+	def autoset_timeconstant(self,periodes=100.):
 		frequency=self.get_frequency()
 		set_timeconstant=float(periodes)/float(frequency)
-		list=[1e-6,3e-6,1e-5,3e-5,1e-4,3e-4,1e-3,5e-3,1e-2,3e-2,1e-1,3e-1,1,3,10,30,100,300,1e+3,3e+3,1e+4,3e+4]
-		index=np.argmin(np.abs(list-set_timeconstant))
+		list_=[1e-5,3e-5,1e-4,3e-4,1e-3,3e-3,1e-2,3e-2,1e-1,3e-1,1,3,10,30,100,300,1e+3,3e+3,1e+4,3e+4]
+		index=np.argmin(np.abs(np.asarray(list_)-set_timeconstant))
 		self.timeconstant=list_[index]
 		self.SerialCommand('OFLT' + str(index))
