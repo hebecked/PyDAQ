71d93cdc40b467931bf976eabf7d041e05f1ae77
hebecked <dustin.hebecker@gmail.com>
2015-10-15 18:15:31 +0200

    Monochromator Comandline interface written

--- bin/monochromator.py
+++ bin/monochromator.py
@@ -169,49 +169,5 @@ class CornerStone260:
     def SetSerialPort(self, port):
         self.serialport = port
 
- 
-if __name__ == '__main__':
 
-    parser=parsers("This is a Monochromator control sub-/main-program for a hardware setup in the Astroparticle group of the Humbolt University of Berlin\nIt is written and maintained by Dustin Hebecker, Mickael Rigault and Daniel Kuesters. (2015)\nFeel free to modify and reuse for non commercial purposes as long as credit is given to the original authors.\n")
-
-    parser.add_argument( "GUI", "-gui", bool, group="config", default=False, help='Set this flag to use a graphical user interface to configure and supervise the DAQ.')
-    parser.add_argument( "Port", "-p", str, group="basics", default=None, help='Sets the com port for the Monochromator.', required=True)
-    parser.add_argument( "Wavelength", "-wvl", float, group="Settings", default=None, help='Allows to set a wavelength manually in units of nm')
-    parser.add_argument( "Filter", "-f", int, group="Settings", default=None, help='Allows to set a Filter manually')
-    parser.add_argument( "Grating", "-g", int, group="Settings", default=None, help='Allows to set a Grating manually')
-    parser.add_argument( "ShutterOpen", "-so", bool, group="Settings", default=False, help='Will open the shutter')
-    parser.add_argument( "ShutterClose", "-sc", bool, group="Settings", default=False, help='Will close the shutter')
-    parser.add_argument( "Info", "-i", float, group="Return", default=None, help='Gives a output on the current monochromator settings') #possibly ad more Return options to obtain informations individually
-
-    arguments=parser.done()
-
-    cs = CornerStone260( port = arguments["Port"])
-
-    if arguments["SCONFIG"]['status']=='set':
-        parser.storeConfig()
-
-    if arguments["GUI"]['val']:
-        print "TODO: Call GUI"
-
-    if arguments["Wavelength"]['status']=='set':
-        cs.Units_NM()
-        cs.GoWave(arguments["Wavelength"]['val'])
-        print "Wavelength set to: " + cs.GetWave() + "nm"
-
-    if arguments["Filter"]['status']=='set':
-        cs.Filter(arguments["Filter"]['val'])
-        #print "Filter set to " + cs.GetFilter() + " with Label " cs.GetFilterLabel(arguments["Filter"]['val'])
-
-    if arguments["Grating"]['status']=='set':
-        cs.Grat(arguments["Grating"]['val'])
-        #print "Grating set to " + cs.GetGrat() + " with Label " cs.GratLabel(arguments["Grating"]['val'])
-
-    if arguments["ShutterOpen"]['val']:
-        cs.ShutterOpen()
-
-    if arguments["ShutterClose"]['val']:
-        cs.ShutterClose()
-
-    if arguments["Info"]['val']:
-        print cs.GetInfo()
     
--- monochromator.py
+++ monochromator.py
@@ -2,60 +2,54 @@
 
 import os, sys
 from bin.parser_wrapper import parsers
-#import GUI
-from bin.DAQ.DAQ_frontend import DAQ_handler
-import multiprocessing
+from bin.monochromator import CornerStone260
+import serial
 import time
+
 
 """
 Definig and reading input parameters and config Files.
 """
-parser=parsers("This Program is meant as a DAQ for a hardware setup in the Astroparticle group of the Humbolt University of Berlin\nIt is written and maintained by Dustin Hebecker, Mickael Rigault and Daniel Kuesters. (2015)\nFeel free to modify and reuse for non commercial purposes as long as credit is given to the original authors.\n")
 
-parser.add_argument( "InstructionFile", "-i", str, group="config", default=None, help='Please supply a instruction file describing all measurement steps. "-ih" will give you a format description.')
-parser.add_argument( "InstructionFileHelp", "-ih", bool, group=None, default=False, help='Gives out the format of the instruction file and exits.')
-parser.add_argument( "OutputFile", "-o", str, group="config", default="results.txt", help='Supply a file name for the results of the measurements')
-parser.add_argument( "GUI", "-g", bool, group="config", default=False, help='Set this flag to use a graphical user interface to configure and supervise the DAQ. Runs won\'t start right away')
-parser.add_argument( "SignalLockInPort", "-slp", str, group="LockIn", default=None, help='Sets the com port for the signal Lock-In.', required=True)
-parser.add_argument( "ReferenceLockInPort", "-rlp", str, group="LockIn", default=None, help='Sets the com port for the reference Lock-In.', required=True)
-parser.add_argument( "XYZ_ScannerPort", "-sp", str, group="XYZ_Scanner", default=None, help='Sets the com port for the XYZ Scanner.', required=True)
-parser.add_argument( "MonochromatorPort", "-mp", str, group="Monochromator", default=None, help='Sets the com port for the Monochromator.', required=True)
-parser.add_argument( "RotationalPlatformPort", "-rp", str, group="RotationalPlatform", default=None, help='Sets the port for the RotationalPlatform.', required=True)
-parser.add_argument( "XYZ_ScannerSafetyZone", "-ssz", list, group="XYZ_Scanner", default=[0,100], help='Please supply a safety range along the x-axis for your experiment in units of percent.', multiargs=True, multiargsn=2, required=True)
+parser=parsers("This is a Monochromator control sub-/main-program for a hardware setup in the Astroparticle group of the Humbolt University of Berlin\nIt is written and maintained by Dustin Hebecker, Mickael Rigault and Daniel Kuesters. (2015)\nFeel free to modify and reuse for non commercial purposes as long as credit is given to the original authors.\n")
 
-#TODo add flags for showing plots in command line mode
+parser.add_argument( "GUI", "-gui", bool, group="config", default=False, help='Set this flag to use a graphical user interface to configure and supervise the DAQ.')
+parser.add_argument( "Port", "-p", str, group="basics", default=None, help='Sets the com port for the Monochromator.', required=True)
+parser.add_argument( "Wavelength", "-wvl", float, group="Settings", default=None, help='Allows to set a wavelength manually in units of nm')
+parser.add_argument( "Filter", "-f", int, group="Settings", default=None, help='Allows to set a Filter manually')
+parser.add_argument( "Grating", "-g", int, group="Settings", default=None, help='Allows to set a Grating manually')
+parser.add_argument( "ShutterOpen", "-so", bool, group="Settings", default=False, help='Will open the shutter')
+parser.add_argument( "ShutterClose", "-sc", bool, group="Settings", default=False, help='Will close the shutter')
+parser.add_argument( "Info", "-i", float, group="Return", default=None, help='Gives a output on the current monochromator settings') #possibly ad more Return options to obtain informations individually
 
 arguments=parser.done()
 
-if arguments["InstructionFileHelp"]['val']:
-	print "TODO: help"
-	exit()
+cs = CornerStone260( port = arguments["Port"])
 
-if not parser.args.SCONFIG==None:
+if arguments["SCONFIG"]['status']=='set':
 	parser.storeConfig()
 
 if arguments["GUI"]['val']:
-#	gui=GUI(parser)
-	pass
-	#exit()
-path2outfile = os.path.split(os.path.abspath(sys.modules['__main__'].__file__))[0]
-outfile = os.path.join(path2outfile, arguments["OutputFile"]["val"] + time.strftime("_%d.%m.%y_%H.%M.result"))
+	print "TODO: Call GUI"
 
-#simple for the beginning, do error handling LATER
-ports={'monochromator':arguments["MonochromatorPort"]['val'], "xyz-scanner":arguments['XYZ_ScannerPort']['val'], 'sLockIn':arguments["SignalLockInPort"]['val'], 'rLockIn':arguments["ReferenceLockInPort"]['val'], 'rotPlatform':arguments["RotationalPlatformPort"]['val']}
-daq=DAQ_handler(arguments["InstructionFile"]['val'], ports, outfile)
+if arguments["Wavelength"]['status']=='set':
+	cs.Units_NM()
+	cs.GoWave(arguments["Wavelength"]['val'])
+	print "Wavelength set to: " + cs.GetWave() + "nm"
 
-while daq.update()=='Running':
-	#print "test"
-	time.sleep(1)
+if arguments["Filter"]['status']=='set':
+	cs.Filter(arguments["Filter"]['val'])
+	#print "Filter set to " + cs.GetFilter() + " with Label " cs.GetFilterLabel(arguments["Filter"]['val'])
 
+if arguments["Grating"]['status']=='set':
+	cs.Grat(arguments["Grating"]['val'])
+	#print "Grating set to " + cs.GetGrat() + " with Label " cs.GratLabel(arguments["Grating"]['val'])
 
-'''
-TODO:
+if arguments["ShutterOpen"]['val']:
+	cs.ShutterOpen()
 
-safety zone (1 dimension for now)
-add option to show plots on comandline
-check (pos) bug on xyz
-check "done" returne bug on XYZ
-wait for positioning on xyz
-'''
+if arguments["ShutterClose"]['val']:
+	cs.ShutterClose()
+
+if arguments["Info"]['val']:
+	print cs.GetInfo()
