f6ecdb7f1e9da4578a30eca29e4b7a7035af997e
hebecked <dustin.hebecker@gmail.com>
2016-04-11 14:34:51 +0200

    Correction attempt for Autogain BUG

--- bin/lockIn.py
+++ bin/lockIn.py
@@ -114,7 +114,12 @@ class lockin:
  		list_=[2e-15,5e-15,1e-14,2e-14,5e-14,1e-13,2e-13,5e-13,1e-12,2e-12,5e-12,1e-11,2e-11,5e-11,1e-10,2e-10,5e-10,1e-9,2e-9,5e-9,1e-8,2e-8,5e-8,1e-7,2e-7,5e-7,1e-6]
  		index_old=2
  		change=20
- 		while change!=0:
+ 		extra=0
+ 		while change!=0 and extra=1:
+ 			if change==0:#check at the end of the cycle if there are some slow changes going on
+ 				extra=1
+ 			else:
+ 				extra=0
  			temp=self.SerialQuery('OUTP?3')
  			#print temp
  			value=float(temp)
@@ -127,7 +132,7 @@ class lockin:
  			self.SerialCommand('SENS' + str(index))
  			change=index_old-index
  			index_old=index
- 			time.sleep(self.timeconstant)
+ 			time.sleep(2*self.timeconstant) #Factor 2 protects also for slow changes
  		return True
 
 	def get_phase(self):
